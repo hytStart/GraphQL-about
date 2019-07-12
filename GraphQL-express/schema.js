c449796c6f8063c3fa03763ac4f2e4c3a1af375a
YiTing Huang <huangyiting@sunlands.com>
2019-07-12 22:28:45 +0800

    koa

---
+++ GraphQL-express/schema.js
@@ -0,0 +1,25 @@
+// schema.js
+import {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLInt
+} from 'graphql';
+
+
+let count = 0;
+
+let schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'RootQueryType',
+        fields: {
+            count: {
+                type: GraphQLInt,
+                resolve: function () {
+                    return count;
+                }
+            }
+        }
+    })
+});
+
+export default schema;
